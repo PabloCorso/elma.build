1f3d98eb1b024f7bd009918f446ec1e66c449e4e
Pablo Corso <pabcorso@gmail.com>
2021-05-21 15:40:15 +0200

    Refactor useElementSize hook

--- src/js/hooks/useElementSize.ts
+++ src/js/hooks/useElementSize.ts
@@ -1,41 +1,5 @@
 import { useLayoutEffect, useState } from "react";
 import useResizeObserver from "@react-hook/resize-observer";
-// import { RefObject, useState, useEffect, useCallback } from "react";
-// import useEventListener from "./useEventListener";
-
-// interface Size {
-//   width: number;
-//   height: number;
-// }
-
-// function useElementSize<T extends HTMLElement = HTMLDivElement>(
-//   elementRef: RefObject<T>
-// ): Size {
-//   const [size, setSize] = useState<Size>({
-//     width: 0,
-//     height: 0,
-//   });
-
-//   // Prevent too many rendering using useCallback
-//   const updateSize = useCallback(() => {
-//     const node = elementRef?.current;
-//     if (node) {
-//       setSize({
-//         width: node.offsetWidth || 0,
-//         height: node.offsetHeight || 0,
-//       });
-//     }
-//   }, [elementRef]);
-
-//   // Initial size on mount
-//   useEffect(() => {
-//     updateSize();
-//   }, []);
-
-//   useEventListener("resize", updateSize);
-
-//   return size;
-// }
 
 function useElementSize<T extends HTMLElement>(
   target: React.RefObject<T>
